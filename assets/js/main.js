3d35968ea08dfbc0b037303c3e40f7358649f295
JimmyNeutron8 <samcapehart@gmail.com>
2019-08-27 15:00:59 -0500

    Cleared out old files

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-// Elements
-const fadeOnLoadElements = document.querySelectorAll('.fade-on-load');
-
-window.addEventListener('load', () => {
-  fadeOnLoadElements.forEach(element => {
-    element.style.opacity = 1;
-  });
-});
